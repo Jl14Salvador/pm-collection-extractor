39cf2ccf3d7c75f378c9c90cb7a3b30a5efe8626
Juan Monterrosxa <jlmonterrosa14@gmail.com>
2017-12-28 09:36:48 -0700

    test(extractor): add vader collection sample tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-expressions */
 const {expect} = require('chai');
-const sampleCollection = require('./sampleCollection.json');
 const extractor = require('../lib/extractor.js');
 const fs = require('fs');
 const rimraf = require('rimraf');
 const _ = require('lodash');
+
+const sampleCollection = require('./sampleCollection.json');
+const sampleCollection2 = require('./Vader.postman_collection.json');
 
 describe('pm-collection-extractor', () => {
 
@@ -32,6 +34,17 @@ describe('pm-collection-extractor', () => {
         expect(fs.existsSync('./test/skywalkerCollection/login/login.json')).to.be.true;
         expect(fs.existsSync('./test/skywalkerCollection/inventory_fetch.json')).to.be.true;
       });
+    });
+
+    it('should extract vader tests into a new folder', () => {
+      return extractor(sampleCollection2)
+        .then(results => {
+          _.forEach(results, result => {
+            expect(result).to.be.undefined;
+          });
+          expect(fs.existsSync('./test/vaderCollection/users/login.json')).to.be.true;
+          expect(fs.existsSync('./test/vaderCollection/partners')).to.be.true;
+        });
     });
 
   });
