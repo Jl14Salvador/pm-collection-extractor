c35480a4b644162ae42202537d1682d4bf0ea67e
Juan Monterrosxa <jlmonterrosa14@gmail.com>
2017-12-28 12:07:27 -0700

    fix(extractor): fix issue where collection breaks after the first folder

--- test/index.spec.js
+++ test/index.spec.js
@@ -25,25 +25,35 @@ describe('pm-collection-extractor', () => {
 
   describe('extractor', () => {
 
-    it.only('should extract tests into a new folder', () => {
+    it('should extract individual tests into directory', () => {
       return extractor(sampleCollection)
-      .then(results => {
-        _.forEach(results, result => {
-          expect(result).to.be.undefined;
+      .then(directoryResut => {
+        _.forEach(directoryResut, folderResult => {
+          _.forEach(folderResult, fileResult => {
+            expect(fileResult).to.be.undefined;
+          });
         });
         expect(fs.existsSync('./test/skywalkerCollection/login/login.json')).to.be.true;
         expect(fs.existsSync('./test/skywalkerCollection/inventory_fetch.json')).to.be.true;
       });
     });
 
-    it.skip('should extract vader tests into a new folder', () => {
+    it('should extract vader tests into a new folder', () => {
       return extractor(sampleCollection2)
-        .then(results => {
-          _.forEach(results, result => {
-            expect(result).to.be.undefined;
+        .then(directoryResult => {
+          _.forEach(directoryResult, folderResult => {
+            _.forEach(folderResult, fileResult => {
+              expect(fileResult).to.be.undefined;
+            });
           });
-          expect(fs.existsSync('./test/vaderCollection/users/login.json')).to.be.true;
+          expect(fs.existsSync('./test/vaderCollection/users')).to.be.true;
           expect(fs.existsSync('./test/vaderCollection/partners')).to.be.true;
+          expect(fs.existsSync('./test/vaderCollection/lease_lender')).to.be.true;
+          expect(fs.existsSync('./test/vaderCollection/fee_defaults')).to.be.true;
+          expect(fs.existsSync('./test/vaderCollection/dealers')).to.be.true;
+          expect(fs.existsSync('./test/vaderCollection/finance_lender')).to.be.true;
+          expect(fs.existsSync('./test/vaderCollection/province_tax_setting')).to.be.true;
+          expect(fs.existsSync('./test/vaderCollection/trims')).to.be.true;
         });
     });
 
